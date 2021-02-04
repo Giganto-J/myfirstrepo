2106958226fd66730a7b6ac0ce5af366ab0aa1f6
Giganto-J <78549834+Giganto-J@users.noreply.github.com>
2021-02-04 07:16:06 -0800

    Create  index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+ 1 contributor
+11 lines (11 sloc)  252 Bytes
+  
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Welcome to my page Giganto T. Joshua</p>
+</body>
+</html>
